83f51ff34e58fdceb23f172e7eeae051b266dbff
James Fulford <james.fulford@outlook.com>
2019-06-16 00:34:48 -0400

    WIP(todos): Rounding out TodoService

--- src/components/todos/TodoService.ts
+++ src/components/todos/TodoService.ts
@@ -2,21 +2,34 @@ import axios from "axios";
 import { Todo, TodoResponse } from "./todo.type";
 
 const _extractData = r => r.data;
-export const TODOS_BASE_URL: string = '/api/todos';
+const BASE_URL: string = '/api/todos';
+const TODO_URL = (todo: TodoResponse): string => `${BASE_URL}/${todo._id}`;
 
-// Methods
+// Core methods
 export const INDEX = (): Promise<TodoResponse[]> => axios({
     method: 'GET',
-    url: TODOS_BASE_URL,
+    url: BASE_URL,
 }).then(_extractData);
 
 export const CREATE = (todo: Todo): Promise<TodoResponse> => axios({
     method: 'POST',
-    url: TODOS_BASE_URL,
+    url: BASE_URL,
     data: todo,
 }).then(_extractData);
 
 export const DELETE = (todo: TodoResponse): Promise<void> => axios({
     method: 'DELETE',
-    url: `${TODOS_BASE_URL}/${todo._id}`
+    url: TODO_URL(todo),
 }).then(_extractData);
+
+export const UPDATE = (todo: TodoResponse): Promise<TodoResponse> => axios({
+    method: 'PUT',
+    url: TODO_URL(todo),
+    data: todo,
+}).then(_extractData);
+
+// Convenience methods
+export const TOGGLE_COMPLETION = (todo: TodoResponse): Promise<TodoResponse> => UPDATE({
+    ...todo,
+    completed: !todo.completed,
+});
